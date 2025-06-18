a845c8b12c82cbd90255d0a50a679dead74777a1
johanh-ansson <165779857+johanh-ansson@users.noreply.github.com>
2025-06-18 23:43:14 +0200

    Create spawnHamster.js

---
+++ spawnHamster.js
@@ -0,0 +1,19 @@
+function spawnHamster() {
+  const gif = document.createElement("img");
+  gif.src = "picz/hamster-dance.gif"; 
+  gif.className = "spinning-hamster";
+  gif.style.position = "fixed";
+  gif.style.left = Math.random() * (window.innerWidth - 80) + "px";
+  gif.style.top = Math.random() * (window.innerHeight - 80) + "px";
+  gif.style.width = "60px";
+  gif.style.zIndex = 9999;
+  gif.style.pointerEvents = "none";
+  document.body.appendChild(gif);
+
+  setTimeout(() => {
+    gif.classList.remove("spin-in");
+    gif.classList.add("spin-out");
+  }, 8000);
+
+  setTimeout(() => gif.remove(), 9000);
+}
